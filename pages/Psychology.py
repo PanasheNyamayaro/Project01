a69aa602d7b1159379e9b78747032fedad4e6eb7
PanasheNyamayaro <montellpanashe43@gmail.com>
2026-02-16 13:45:36 +0200

    Update Psychology.py

--- pages/Psychology.py
+++ pages/Psychology.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Psycology", page_icon="", layout="centered")
 st.subheader("Technical analysis")
+
+adilo_url = "https://adilo.bigcommand.com/watch/xxlIIoej"
+
+components.iframe(adilo_url, height=450, scrolling=False)
