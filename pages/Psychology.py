4dd95596e1accb7adf244e9b181410077e99b9bf
PanasheNyamayaro <montellpanashe43@gmail.com>
2026-02-16 14:03:02 +0200

    Update Psychology.py

--- pages/Psychology.py
+++ pages/Psychology.py
@@ -2,9 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Psycology", page_icon="", layout="centered")
-st.subheader("")
+st.subheader("phycology")
 
 adilo_url = "https://adilo.bigcommand.com/watch/xxlIIoej"
 
 components.iframe(adilo_url, height=450, scrolling=False)
-st.subheader("Head and shoulder pattern")
+st.write("Head and shoulder pattern")
